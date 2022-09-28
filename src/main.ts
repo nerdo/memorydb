7f0e00c016ab86bdc847b1ee9ad545773c0664e9
Dannel Albert <cuebix@gmail.com>
2022-09-27 22:12:15 -0400

    chore: initial commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <img src="/nerdo-memory-db.svg" class="logo vanilla" alt="Nerdo Memory DB" />
+  </div>
+`
